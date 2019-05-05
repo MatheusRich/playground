3942f81fa90353ba5bf96d0bb5568aa79bfc6cbd
Matheus Richard <matheusrichardt@gmail.com>
2019-05-05 16:51:32 -0300

    Updates table.cpp

--- TABLE/table.cpp
+++ TABLE/table.cpp
@@ -7,6 +7,11 @@
 
 using namespace std;
 
+bool is_terminal(char c) {
+  return islower(c);
+}
+
+
 int main(){
   /* ********** LER ENTRADA ********** */
   int nvar;
@@ -31,6 +36,8 @@ int main(){
     tabela_de_producoes[aux] = aux3;
   }
 
+  cout << "\n";
+
   cout << "TABELA DE PRODUÇÕES\n";
   for(auto i : tabela_de_producoes){
     cout << i.variable << " -> (";
@@ -41,7 +48,7 @@ int main(){
     cout << output.erase(0,2) <<")\n";
   }
 
-    cout << "\n";
+  cout << "\n";
 
 
   /* ********** LER ENTRADA ********** */
@@ -67,7 +74,59 @@ int main(){
     }
     cout << "]\n";
   }
-  
+
+  map<string, vector<pair<char, string>>> tabela2;
+
+  for(auto row:tabela1) {
+    int n_elements = row.second.size();
+    tabela2[row.variable] = row.second; // copies tabela1 vector to table 2
+    for (int i = 0; i< n_elements; i++) {
+      char current = tabela2[row.variable][i].first; 
+      if (not is_terminal(current)) {
+        string scurrent = string(1, current);
+        for (auto j : tabela1[scurrent]) {
+          string from = tabela2[row.variable][i].second;
+          auto asdf = make_pair(j.first, from);
+          tabela2[row.variable].push_back(asdf);
+          n_elements++;      
+        }
+      }
+    }
+  }
+
+  cout << "\n";
+
+  cout << "TABELA 2\n";
+  for(auto row : tabela2){
+    cout << row.first << " -> [";
+    for(auto producao : row.second){
+      cout << "(" << producao.first;
+      cout << ", " << producao.second << ")";
+    }
+    cout << "]\n";
+  }
+
+  map<string, vector<pair<char, string>>> tabela_final;
+
+  for(auto row : tabela2){
+    for(auto producao : row.producoes){
+      if(is_terminal(producao.first)) {
+        tabela_final[row.variable].push_back(producao);
+      }
+    }
+  }
+
+  cout << "\n";
+
+  cout << "TABELA FINAL\n";
+  for(auto row : tabela_final){
+    cout << row.first << " -> [";
+    for(auto producao : row.second){
+      cout << "(" << producao.first;
+      cout << ", " << producao.second << ")";
+    }
+    cout << "]\n";
+  }
+
   return 0;
 }
-
