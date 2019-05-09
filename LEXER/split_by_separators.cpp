917d96cc70e32c95c68b283fb2e7dd21aa52e3a6
Matheus Richard <matheusrichardt@gmail.com>
2019-05-09 00:17:53 -0300

    Adds split by separators on LEXER

---
+++ LEXER/split_by_separators.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <map>
+
+#define SEPARATORS_BEGIN 1
+#define SEPARATORS_END 5
+
+bool is_in_range(int n, int min, int max) {
+  return (n >= min && n <= max);
+}
+
+
+std::string char_to_str(char c) {
+  return std::string(1, c);
+}
+
+bool is_separator(char c, std::map<std::string, int> separators) { // maybe input should be a string
+  return separators.find(char_to_str(c)) != separators.end();
+}
+bool is_separator2(char c, std::map<std::string, int> separators) { // This allows having only one structure saving termianals and flaging the separators
+  return (not(separators.find(char_to_str(c)) == separators.end())
+          || is_in_range(separators[char_to_str(c)], SEPARATORS_BEGIN, SEPARATORS_END));
+}
+
+std::string split_by_separators(std::string input, std::map <std::string, int> separators) {
+  std::string output = "";
+
+  for(char c : input) {
+    if (is_separator(c, separators)) {
+      if(output.size() == 0) // single separator, no word
+        output += c;
+      break;
+    } else {
+      output += c;
+    }
+  }
+  
+  return output;
+}
+
+int main() {
+  std::map <std::string, int> separators;
+  int i = 1;
+  separators["("] = i++;
+  separators[")"] = i++;
+  separators[","] = i++;
+  separators[" "] = i++;
+  separators["\n"] = i++; // Is this needed?
+
+
+  std::string input = "program(a, b);";
+
+  while (input.size() > 0) {
+    std::string separated = split_by_separators(input, separators);
+    input = input.substr(separated.size(), input.size() - 1);
+    puts(("Token: '" + separated + "'").c_str());
+  }
+
+  return 0;
+}
